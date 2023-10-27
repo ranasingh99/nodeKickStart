2f3ca2a1273e90210b32e7a336a64f678620d63b
ranasingh99 <sanketverma852@gmail.com>
2023-10-27 12:31:20 +0530

    gettig the url

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-var server = require('http');
-server.createServer(engion).listen(3000)
+var http = require('http');
+const { type } = require('os');
+var server = http.createServer(engion)
+server.listen(3000,function(){
+    console.log('srver was hit by a request')
+})
 
 function engion(request,response){
+
     response.writeHead(200,{'content-type':'text/plain'})
-    response.end('Hey there, from server')
+    response.end('Hey there from the server'+ request.url)
+  //console.log(response)
 }
